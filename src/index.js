4633b3e4ab1ea46794abe615648330af3e77acd4
rsgen <rsgen2015@gmail.com>
2023-03-19 15:49:28 +0300

    API, данные из базы, полноценный поиск, данные пользователя, обновление лайков

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./components/app";
-import { AntApp } from "./components/app-ant";
-import { AppMui } from './components/app-mui';
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<StrictMode><AppMui /></StrictMode>);
+root.render(<App />);
